ea7ca7761ad12842479cc40fb8295f140cc7551f
harshit-python <hsaxena1998@gmail.com>
2024-01-20 01:10:04 +0530

    my first java script program

---
+++ first.js
@@ -0,0 +1,37 @@
+let fullName = "Harsh Saxena";
+let age = 25;
+console.log(fullName);
+console.log(typeof fullName);
+console.log(age);
+console.log(typeof age);
+
+
+// using object
+const programmer = {
+    fullName : "Harsh Saxena",
+    age : 25,
+    isActive : true
+};
+console.log(programmer);
+console.log(programmer["fullName"]);
+console.log(programmer.fullName);
+
+
+// product object
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+console.log(product);
+
+
+// social media profile object
+const profile = {
+    userName: "harsh.py_",
+    isFollow: true,
+    followers: 500,
+    following: 200,
+};
+console.log(profile)
